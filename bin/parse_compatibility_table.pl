07b17367bb0bf0eeb561447425540850fcc66145
fluks <fluks.github@gmail.com>
2019-01-26 05:49:11 +0200

    Use browser compat data
    Use Mozilla's browser compatibility data instead of parsing the same
    data from MDN website as we used to.
    
    https://github.com/mdn/browser-compat-data/

--- bin/parse_compatibility_table.pl
+++ bin/parse_compatibility_table.pl
@@ -3,40 +3,81 @@ use strict;
 use warnings;
 use feature qw(say signatures);
 no warnings 'experimental::signatures';
-use Mojo::UserAgent;
+use English;
+use File::Basename;
+use JSON;
+use File::Spec;
 use Data::Dumper;
 
-my $url = 'https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Browser_support_for_JavaScript_APIs';
-my $dom = Mojo::UserAgent->new->get($url)->res->dom;
-my $map = {};
+my $program = basename($0);
+my $compat_dir = $ARGV[0] || die <<USAGE;
+Usage: $program BROWSER_COMPAT_DATA_DIRECTORY
+USAGE
 
-for my $h2 ($dom->find('h2')->each) {
-    my $lib = $h2->attr('id');
-    next unless $lib;
-    my $table = $dom->find("h2[id=$lib] + table")->first;
-    next unless $table;
+opendir(my $dh, $compat_dir) || die "Can't open $compat_dir: $!";
+my $obj = {};
+while ((my $dir_entry = readdir $dh)) {
+    $dir_entry = File::Spec->catfile($compat_dir, $dir_entry);
+    next unless -f $dir_entry;
 
-    my @browsers = map { $_->text } $table->find('th')->each;
-    next unless @browsers;
-    shift @browsers;
-    for my $tr ($table->find('tbody > tr')->each) {
-        my @tds = $tr->find('td')->each;
-        my $symbol = (shift @tds)->children->first->text;
-        for (my $i = 0; $i < @tds; $i++) {
-            my $child = $tds[$i]->children->first;
-            my $support;
-            if ($child) {
-                $support = $child->text;
-            }
-            else {
-                $support = $tds[$i]->text;
-            }
-            
-            $support =~ s/\W//g;
-            $map->{"${lib}.${symbol}"}->{$browsers[$i]} = $support;
-        }
-    }
+    open(my $fh, '<', $dir_entry) || do {
+        warn "Can't open $dir_entry: $!";
+        next;
+    };
+    undef $INPUT_RECORD_SEPARATOR;
+    my $json = decode_json(<$fh>);
+
+    my $lib_json = $json->{webextensions}->{api};
+    my $lib = (keys %$lib_json)[0];
+
+    find_symbols($lib_json, '', $obj);
 }
 
 $Data::Dumper::Sortkeys = 1;
-print Dumper $map;
+say Dumper($obj);
+
+sub find_symbols {
+    my ($json, $s, $obj) = @_;
+
+    return unless ref $json eq 'HASH';
+
+    my @keys = keys %$json;
+    for my $k (@keys) {
+        if ($k eq '__compat') {
+            return unless exists $json->{__compat}->{mdn_url};
+
+            my $browser_json = $json->{__compat}->{support};
+            my @browsers = keys %{ $browser_json };
+
+            for my $brow (@browsers) {
+                my $brow_json = $browser_json->{$brow};
+                if (ref $brow_json eq 'ARRAY') {
+                    for my $version (@$brow_json) {
+                        if (exists $version->{alternative_name}) {
+                            $obj->{ $version->{alternative_name} }->{$brow} =
+                                version_to_value($version->{version_added});
+                        }
+                        else {
+                            $obj->{$s}->{$brow} =
+                                version_to_value($version->{version_added});
+                        }
+                    }
+                }
+                else {
+                    $obj->{$s}->{$brow} =
+                        version_to_value($brow_json->{version_added});
+                }
+            }
+        }
+
+        my $symbol = $s ? "$s.$k" : $k;
+        find_symbols($json->{$k}, $symbol, $obj);
+    }
+}
+
+sub version_to_value($version) {
+    if (JSON::is_bool($version)) {
+        return JSON::true == $version ? 'Yes' : 'No';
+    }
+    return $version;
+}
